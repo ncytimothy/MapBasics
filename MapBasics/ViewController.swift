0a68d3b362107806df699470c27c46c0c99bdd01
Tim Ng <timothyng2008@me.com>
2019-01-14 20:48:50 +0800

    Initial commit

---
+++ MapBasics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapBasics
+//
+//  Created by Tim Ng on 1/14/19.
+//  Copyright © 2019 timothyng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
