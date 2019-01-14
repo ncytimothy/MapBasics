47b2e2ed82b388dc45d7ea8d285c557e887719f4
Tim Ng <timothyng2008@me.com>
2019-01-14 21:00:55 +0800

    feat: Add MapView

--- MapBasics/ViewController.swift
+++ MapBasics/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let mapView: MKMapView = {
+            let mapView = MKMapView()
+            mapView.translatesAutoresizingMaskIntoConstraints = false
+            return mapView
+        }()
+        
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+                mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                mapView.heightAnchor.constraint(equalTo: view.heightAnchor)
+            ])
+        
+        
+        
+        
     }
 
 
